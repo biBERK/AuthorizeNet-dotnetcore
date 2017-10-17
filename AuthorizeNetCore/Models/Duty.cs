7f2211d756bf8561b40b6245dd1831f2af7ce8fc
jasongaylord <jason@jasongaylord.com>
2017-10-16 23:23:26 -0400

    Added Duty model

---
+++ AuthorizeNetCore/Models/Duty.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace AuthorizeNetCore.Models
+{
+    public class Duty
+    {
+        [JsonProperty(PropertyName = "amount")]
+        public string Amount { get; set; }
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+        [JsonProperty(PropertyName = "description")]
+        public string Description { get; set; }
+    }
+}
