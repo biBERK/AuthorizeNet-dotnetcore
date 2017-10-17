d9c32996a38f5f173870c1b68a2984c64b2c094a
jasongaylord <jason@jasongaylord.com>
2017-10-16 23:12:31 -0400

    Added Payment

---
+++ AuthorizeNetCore/Models/Payment.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace AuthorizeNetCore.Models
+{
+    public class Payment
+    {
+        [JsonProperty(PropertyName = "creditCard")]
+        public CreditCard CreditCard { get; set; }
+    }
+}
