067f8c9c22594d1411706474dc79481f0ba19165
jasongaylord <jason@jasongaylord.com>
2017-10-16 23:11:50 -0400

    Added CreateTransactionRequest

---
+++ AuthorizeNetCore/Models/CreateTransactionRequest.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace AuthorizeNetCore.Models
+{
+    public class CreateTransactionRequest
+    {
+        [JsonProperty(PropertyName = "merchantAuthentication")]
+        public MerchantAuthentication MerchantAuthentication { get; set; }
+        [JsonProperty(PropertyName = "refId")]
+        public string ReferenceId { get; set; }
+        [JsonProperty(PropertyName = "transactionRequest")]
+        public TransactionRequest TransactionRequest { get; set; }
+    }
+}
