48c3f0705bb3c6e19506a30eedc9bf091cde0cf2
jasongaylord <jason@jasongaylord.com>
2017-10-16 23:20:55 -0400

    Added Tax model

---
+++ AuthorizeNetCore/Models/Tax.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace AuthorizeNetCore.Models
+{
+    public class Tax
+    {
+        [JsonProperty(PropertyName = "amount")]
+        public string Amount { get; set; }
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+        [JsonProperty(PropertyName = "description")]
+        public string Description { get; set; }
+    }
+}
