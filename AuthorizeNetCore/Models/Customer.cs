5baeaa9bb84c53bb532e7ca5b39d4377d44cad55
jasongaylord <jason@jasongaylord.com>
2017-10-16 23:30:19 -0400

    Added Customer model

---
+++ AuthorizeNetCore/Models/Customer.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace AuthorizeNetCore.Models
+{
+    public class Customer
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+    }
+}
