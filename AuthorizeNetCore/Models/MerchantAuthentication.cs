8c3d334966f36cc12e930a63f0e25679816d3339
jasongaylord <jason@jasongaylord.com>
2017-10-16 23:12:07 -0400

    Added MerchantAuthentication

---
+++ AuthorizeNetCore/Models/MerchantAuthentication.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace AuthorizeNetCore.Models
+{
+    public class MerchantAuthentication
+    {
+        [JsonProperty(PropertyName = "name")]
+        public string LoginId { get; set; }
+        [JsonProperty(PropertyName = "transactionKey")]
+        public string TransactionKey { get; set; }
+    }
+}
