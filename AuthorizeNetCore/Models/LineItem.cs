25280d4b273b73bc1119b2e6da112559e22dc70c
jasongaylord <jason@jasongaylord.com>
2017-10-16 23:18:28 -0400

    Added LineItem

---
+++ AuthorizeNetCore/Models/LineItem.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace AuthorizeNetCore.Models
+{
+    public class LineItem
+    {
+        [JsonProperty(PropertyName = "itemId")]
+        public string ItemId { get; set; }
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+        [JsonProperty(PropertyName = "description")]
+        public string Description { get; set; }
+        [JsonProperty(PropertyName = "quantity")]
+        public string Quantity { get; set; }
+        [JsonProperty(PropertyName = "unitPrice")]
+        public string UnitPrice { get; set; }
+    }
+}
