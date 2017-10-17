93484bc0206daecf50a390124186598cfc2800a6
jasongaylord <jason@jasongaylord.com>
2017-10-16 23:14:08 -0400

    Added CreditCard

---
+++ AuthorizeNetCore/Models/CreditCard.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace AuthorizeNetCore.Models
+{
+    public class CreditCard
+    {
+        [JsonProperty(PropertyName = "cardNumber")]
+        public string CardNumber { get; set; }
+        [JsonProperty(PropertyName = "expirationDate")]
+        public string ExpirationDate { get; set; }
+        [JsonProperty(PropertyName = "cardCode")]
+        public string CardCode { get; set; }
+    }
+}
