65930e8afda148daf6afdc096e05a6cf5e2edd8d
jasongaylord <jason@jasongaylord.com>
2017-10-16 23:36:56 -0400

    Added Setting and TransactionSettings models

---
+++ AuthorizeNetCore/Models/Setting.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace AuthorizeNetCore.Models
+{
+    public class Setting
+    {
+        [JsonProperty(PropertyName = "settingName")]
+        public string SettingName { get; set; }
+        [JsonProperty(PropertyName = "settingValue")]
+        public string SettingValue { get; set; }
+    }
+}
---
+++ AuthorizeNetCore/Models/TransactionSettings.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace AuthorizeNetCore.Models
+{
+    public class TransactionSettings
+    {
+        [JsonProperty(PropertyName = "settingName")]
+        public Setting Setting { get; set; }
+    }
+}
