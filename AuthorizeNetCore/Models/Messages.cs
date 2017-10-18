2b49b531848f7bad0f50ad7c4ec353fddfc6bc0c
jasongaylord <jason@jasongaylord.com>
2017-10-17 21:29:46 -0400

    Added Messages model

---
+++ AuthorizeNetCore/Models/Messages.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace AuthorizeNetCore.Models
+{
+    public class Messages
+    {
+        [JsonProperty(PropertyName = "resultCode")]
+        public string ResultCode { get; set; }
+        [JsonProperty(PropertyName = "message")]
+        public ResultMessage[] ResultMessages { get; set; }
+    }
+}
