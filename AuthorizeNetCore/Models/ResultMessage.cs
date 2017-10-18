a05ffd74ae3d014f217449d8851216d162ff93ba
jasongaylord <jason@jasongaylord.com>
2017-10-17 21:31:43 -0400

    Added ResultMessage model.

---
+++ AuthorizeNetCore/Models/ResultMessage.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace AuthorizeNetCore.Models
+{
+    public class ResultMessage
+    {
+        [JsonProperty(PropertyName = "code")]
+        public string Code { get; set; }
+        [JsonProperty(PropertyName = "text")]
+        public string Text { get; set; }
+    }
+}
