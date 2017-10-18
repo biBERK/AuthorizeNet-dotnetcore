b53e52b12d0b70f9c1bfadd3734ebbcc5415c8eb
jasongaylord <jason@jasongaylord.com>
2017-10-17 21:43:09 -0400

    Added the most basic transaction request as well as the most robust. Added scaffolding for transaction response.

---
+++ AuthorizeNetCore/CreditCard.cs
@@ -0,0 +1,60 @@
+﻿using AuthorizeNetCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthorizeNetCore
+{
+    public class CreditCard
+    {
+        private readonly string _authorizeNetUrl;
+        private readonly string _apiLoginId;
+        private readonly string _transactionKey;
+
+        public CreditCard(string authorizeNetUrl, string apiLoginId, string transactionKey)
+        {
+            _authorizeNetUrl = authorizeNetUrl;
+            _apiLoginId = apiLoginId;
+            _transactionKey = transactionKey;
+        }
+
+        public TransactionResponse Charge(ChargeCreditCardRequest chargeCreditCardRequest)
+        {
+            return new TransactionResponse();
+        }
+
+        public TransactionResponse Charge(string nounce, string referenceId, decimal amount, string customerId, string customerIpAddress)
+        {
+            // Build the request
+            var chargeCreditCardRequest = new ChargeCreditCardRequest
+            {
+                CreateTransactionRequest = new CreateTransactionRequest
+                {
+                    MerchantAuthentication = new MerchantAuthentication { LoginId = _apiLoginId, TransactionKey = _transactionKey },
+                    ReferenceId = referenceId,
+                    TransactionRequest = new TransactionRequest
+                    {
+                        Amount = amount.ToString(),
+                        BillTo = new CustomerContact(),
+                        Customer = new Customer { Id = customerId },
+                        CustomerIP = customerIpAddress,
+                        Duty = new Duty(),
+                        LineItems = new LineItems { LineItem = new LineItem[0] },
+                        Payment = new Payment
+                        {
+                            OpaqueData = new OpaqueData { NonceValue = nounce }
+                        },
+                        PoNumber = "",
+                        Shipping = new Shipping(),
+                        ShipTo = new CustomerContact(),
+                        Tax = new Tax(),
+                        TransactionSettings = new TransactionSettings { Setting = new Setting[0] },
+                        UserFields = new UserFields { UserField = new UserField[0] }
+                    }
+                }
+            };
+
+            return Charge(chargeCreditCardRequest);
+        }
+    }
+}
