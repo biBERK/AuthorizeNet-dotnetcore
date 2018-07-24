8578f27538888c6530ab5df2d10bbcf24831ccd5
Linette Green <Linette.Green@biberk.com>
2018-07-24 11:12:52 -0400

    Add Order and Invoice number model for Credit Card transaction request. Version 1.0.4

---
+++ AuthorizeNetCore/Models/Order.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace AuthorizeNetCore.Models
+{
+	public class Order
+    {
+		[JsonProperty(PropertyName = "invoiceNumber")]
+		public string InvoiceNumber { get; set; }
+		[JsonProperty(PropertyName = "description")]
+		public string Description { get; set; }
+	}
+}
